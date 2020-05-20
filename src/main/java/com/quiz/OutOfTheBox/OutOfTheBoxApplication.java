14c77719b7ed4d2c21692901f88a8c311fb537ca
Maishaa Ameera Rifani <maishaaameerarifani@Maishaas-MacBook-Pro.local>
2020-05-20 14:33:00 +0700

    Initiate project

---
+++ src/main/java/com/quiz/OutOfTheBox/OutOfTheBoxApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.OutOfTheBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutOfTheBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutOfTheBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quiz/OutOfTheBox/OutOfTheBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.OutOfTheBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutOfTheBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
