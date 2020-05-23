a5f343824eb621cbab195f465751895fbfd8695a
Raniah Nur Hanami <raniahnurhanami@Raniahs-Xiaomi.local>
2020-05-23 11:45:50 +0700

    fileBaru

--- src/main/java/com/quiz/OutOfTheBox/Quiz.java
+++
@@ -1,4 +0,0 @@
-package com.quiz.OutOfTheBox;
-public class Quiz {
-    
-}
---
+++ src/main/java/com/quiz/OutOfTheBox/model/Pemain.java
@@ -0,0 +1,44 @@
+package com.quiz.OutOfTheBox.model;
+
+import java.util.ArrayList;
+
+
+public class Pemain {
+    private static ArrayList<Pemain> daftarPemain = new ArrayList<Pemain>();
+    private String nama;
+    private int skor = 0;
+    private int nyawa = 5;
+    protected boolean gameOver = false;
+
+    public static ArrayList<Pemain> getDaftarPemain() {
+        return daftarPemain;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getSkor() {
+        return skor;
+    }
+
+    public void setSkor(int skor) {
+        this.skor = skor;
+    }
+
+    public int getNyawa() {
+        return nyawa;
+    }
+
+    public void kurangiNyawa() {
+        nyawa--;
+    }
+
+    public void setNyawa(int nyawa) {
+        this.nyawa = nyawa;
+    }
+}
