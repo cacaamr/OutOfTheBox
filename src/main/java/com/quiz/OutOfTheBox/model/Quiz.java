11ea068ac9f8ce10df734cdbe7bdf6513404311f
Raniah Nur Hanami <raniahnurhanami@Raniahs-Xiaomi.local>
2020-05-26 20:52:37 +0700

    test

--- src/main/java/com/quiz/OutOfTheBox/model/Quiz.java
+++ src/main/java/com/quiz/OutOfTheBox/model/Quiz.java
@@ -9,27 +9,14 @@ public class Quiz {
     // ArrayList<String> questarr;
 
     public Quiz() {
-        // question.getListQuestion();
         randomQuestion = new Random();
         qna = new QnA();
-        // questarr = new ArrayList<>();
-        // questarr.add("siapa?");
-        // questarr.add("apa?");
 
     }
 
     public String getRandomQuestion() {
         int index = randomQuestion.nextInt(qna.getListQuestion().size());
         String quest = qna.getListQuestion().get(index);
-        // System.out.println(index);
-        // System.out.println("halo " + quest);
         return quest;
     }
-
-    public static void main(String[] args) {
-        Quiz quiz = new Quiz();
-        System.out.println(quiz.getRandomQuestion());
-
-    }
-
 }
