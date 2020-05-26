6fb961079a670a2aa013f4d39c40a0cf96e8a07b
Raniah Nur Hanami <raniahnurhanami@Raniahs-Xiaomi.local>
2020-05-26 20:37:51 +0700

    yey

--- src/main/java/com/quiz/OutOfTheBox/model/QnA.java
+++ src/main/java/com/quiz/OutOfTheBox/model/QnA.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 
-public class QnA {
+class QnA {
 
     BufferedReader br;
     private ArrayList<String> listQuestion = new ArrayList<String>();
@@ -20,24 +20,23 @@ public class QnA {
 
     public void readFileQuestion(){
         try{
-            br = new BufferedReader(new FileReader("/Users/maishaaameerarifani/Desktop/OutOfTheBox/src/main/java/com/quiz/OutOfTheBox/model/question.txt"));
+            br = new BufferedReader(new FileReader("question.txt"));
             String txt = br.readLine();
     
             while(txt != null){
+                System.out.println(txt);
                 listQuestion.add(txt);
                 txt = br.readLine();
             }
-    
-            System.out.println(listQuestion);
         } catch(IOException e){
             e.printStackTrace();
-        }
+        }System.out.println(listQuestion);
         
     }
 
     public void readFileAnswer(){
         try{
-            br = new BufferedReader(new FileReader("/Users/maishaaameerarifani/Desktop/OutOfTheBox/src/main/java/com/quiz/OutOfTheBox/model/answer.txt"));
+            br = new BufferedReader(new FileReader("answer.txt"));
             String txt = br.readLine();
     
             while(txt != null){
@@ -51,33 +50,8 @@ public class QnA {
         }
         
     }
-
-
-
-    // public void listQnA() throws IOException{
-    //     // try{
-    //         BufferedReader br = new BufferedReader(new FileReader("E:\\qna.txt"));
-    //         String txt = br.readLine();
-    //         ArrayList<String> listQuestion = new ArrayList<String>();
-    //         ArrayList<String> listAnswer = new ArrayList<String>();
-    //         // ArrayList<String> listQna= new ArrayList<String>();
-    
-    //         while(txt != null){
-    //             String [] listQna = txt.split("//");
-    //             listQuestion.add(listQna[0]);
-    //             listAnswer.add(listQna[1]);
-    //             System.out.println(listQna);
-    //             txt = br.readLine();
-    //         }
-    //     // }catch(IOException e){
-    //     //     e.printStackTrace();
-    //     // }catch(FileNotFoundException e){
-    //     //     e.printStackTrace();
-    //     // }
-    // }
-
-    // public void printzz(){
-    //     String str = "HelloWorld";
-    // }
-
+    public static void main(String[] args) {
+        QnA q = new QnA();
+        q.getListAnswer();
+    }
 }
