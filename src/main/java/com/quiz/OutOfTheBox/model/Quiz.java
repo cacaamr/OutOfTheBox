a28265bb20debf396671c927615046c6058ab857
Maishaa Ameera Rifani <maishaaameerarifani@Maishaas-MacBook-Pro.local>
2020-05-26 17:46:46 +0700

    push ke master

---
+++ src/main/java/com/quiz/OutOfTheBox/model/Quiz.java
@@ -0,0 +1,36 @@
+package com.quiz.OutOfTheBox.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Quiz {
+    private Random randomQuestion;
+    //ArrayList<String> questarr;
+
+    public Quiz(){
+        //question.getListQuestion();
+        randomQuestion = new Random();
+        //questarr = new ArrayList<>();
+        //questarr.add("siapa?");
+        //questarr.add("apa?");
+
+    }
+
+    public String getRandomQuestion(){
+        QnA question = new QnA();
+        int index = randomQuestion.nextInt(question.getListQuestion().size());
+        String quest = question.getListQuestion().get(index);
+        System.out.println(quest);
+        //System.out.println(index);
+        //System.out.println("halo " + quest);
+        return quest;
+    }
+
+
+    public static void main(String[] args) {
+        Quiz question = new Quiz();
+        question.getRandomQuestion();
+
+    }
+
+}
